9002e82f14f0450631079d9dc09ef973cc44598a
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-07-21 12:47:10 -0400

    Moved API Spec to controller interface instead of impl

--- src/main/java/com/dcp/chatgpt_service/v1/controllers/ChatGPTController.java
+++ src/main/java/com/dcp/chatgpt_service/v1/controllers/ChatGPTController.java
@@ -8,23 +8,30 @@ import com.dcp.chatgpt_service.v1.models.chat.MultiChatResponse;
 import com.dcp.chatgpt_service.v1.models.image.ImageFormat;
 import com.dcp.chatgpt_service.v1.models.image.ImageSize;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/v1/chatgpt")
 public interface ChatGPTController {
 
+	@PostMapping("/sendMessage")
 	ResponseEntity<String> sendMessage(@RequestBody String message);
 
+	@PostMapping("/sendChatRequest")
 	ResponseEntity<ChatResponse> sendChatRequest(@RequestBody ChatRequest chatRequest);
 
+	@PostMapping("/multiChat")
 	ResponseEntity<String> multiChat(@RequestBody List<MultiChatMessage> messages);
 
+	@PostMapping("/multiChatRequest")
 	ResponseEntity<MultiChatResponse> multiChatRequest(@RequestBody MultiChatRequest multiChatRequest);
 
+	@PostMapping("/generateImage")
 	ResponseEntity<String> generateImage(@RequestBody String prompt);
 
+	@PostMapping("/generateImages")
 	ResponseEntity<List<String>> generateImages(@RequestParam String prompt,
 												@RequestParam Integer n,
 												@RequestParam ImageSize size,
--- src/main/java/com/dcp/chatgpt_service/v1/controllers/ChatGPTControllerImpl.java
+++ src/main/java/com/dcp/chatgpt_service/v1/controllers/ChatGPTControllerImpl.java
@@ -15,48 +15,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1/chatgpt")
+
 public class ChatGPTControllerImpl implements ChatGPTController {
 
 	private final ChatGPTService chatGPTService;
 
-	@Autowired
 	public ChatGPTControllerImpl(ChatGPTService chatGPTService) {
 		this.chatGPTService = chatGPTService;
 	}
 
-	@PostMapping("/sendMessage")
 	@Override
 	public ResponseEntity<String> sendMessage(@RequestBody String message) {
 		return ResponseEntity.ok(chatGPTService.sendMessage(message));
 	}
 
-	@PostMapping("/sendChatRequest")
 	@Override
 	public ResponseEntity<ChatResponse> sendChatRequest(@RequestBody ChatRequest chatRequest) {
 		return ResponseEntity.ok(chatGPTService.sendChatRequest(chatRequest));
 	}
 
-	@PostMapping("/multiChat")
 	@Override
 	public ResponseEntity<String> multiChat(@RequestBody List<MultiChatMessage> messages) {
 		return ResponseEntity.ok(chatGPTService.multiChat(messages));
 	}
 
-	@PostMapping("/multiChatRequest")
 	@Override
 	public ResponseEntity<MultiChatResponse> multiChatRequest(@RequestBody MultiChatRequest multiChatRequest) {
 		return ResponseEntity.ok(chatGPTService.multiChatRequest(multiChatRequest));
 	}
 
-	@PostMapping("/generateImage")
 	@Override
 	public ResponseEntity<String> generateImage(@RequestBody String prompt) {
 		return ResponseEntity.ok(chatGPTService.generateImage(prompt));
 	}
 
-	@PostMapping("/generateImages")
 	@Override
 	public ResponseEntity<List<String>> generateImages(@RequestParam String prompt,
 													   @RequestParam Integer n,
